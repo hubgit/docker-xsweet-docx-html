222ca968e67b0393e7774ea9aca26f0f0267ce82
Alf Eaton <eaton.alf@gmail.com>
2017-12-21 09:23:03 +0000

    Fetch and use only the required XSL files

---
+++ make.php
@@ -0,0 +1,46 @@
+<?php
+
+$urls = [
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/docx-extract/collapse-paragraphs.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/docx-extract/docx-html-extract.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/docx-extract/handle-notes.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/docx-extract/join-elements.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/docx-extract/scrub.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/html-polish/final-rinse.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/list-promote/itemize-lists.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/list-promote/mark-lists.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/header-promote/make-header-escalator-xslt.xsl',
+    'https://gitlab.coko.foundation/XSweet/XSweet/raw/ink-api-publish/applications/header-promote/digest-paragraphs.xsl',
+    'https://gitlab.coko.foundation/XSweet/editoria_typescript/raw/ink-api-publish/p-split-around-br.xsl',
+    'https://gitlab.coko.foundation/XSweet/editoria_typescript/raw/ink-api-publish/editoria-notes.xsl',
+    'https://gitlab.coko.foundation/XSweet/editoria_typescript/raw/ink-api-publish/editoria-basic.xsl',
+    'https://gitlab.coko.foundation/XSweet/editoria_typescript/raw/ink-api-publish/editoria-reduce.xsl',
+];
+
+$outputDir = __DIR__ . '/src/xsl';
+
+if (!file_exists($outputDir)) {
+    mkdir($outputDir);
+}
+
+foreach ($urls as $url) {
+    $input = fopen($url, 'r');
+    if (!$input) {
+        exit("Failed opening $url\n");
+    }
+
+    $outputPath = $outputDir . '/' . basename($url);
+    $output = fopen($outputPath, 'w');
+    if (!$output) {
+        exit("Failed opening $outputPath\n");
+    }
+
+    print("Copying $url to $outputPath\n");
+    $result = stream_copy_to_stream($input, $output);
+    if (!$result) {
+        exit("Failed copying $url to $outputPath\n");
+    }
+
+    fclose($input);
+    fclose($output);
+}
